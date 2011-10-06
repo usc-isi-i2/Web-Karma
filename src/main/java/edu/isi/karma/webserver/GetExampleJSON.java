fc12a8b7d7beafd7e2281f45412a15d12f2565d6
Pedro Szekely <szekely1401@gmail.com>
2011-10-05 19:11:17 -0700

    More samples to load by default.

--- src/main/java/edu/isi/karma/webserver/GetExampleJSON.java
+++ src/main/java/edu/isi/karma/webserver/GetExampleJSON.java
@@ -43,11 +43,12 @@ public class GetExampleJSON extends HttpServlet {
 	//	SampleDataFactory.createFlatWorksheet(workspace, 2, 2);
 		//SampleDataFactory.createFromJsonTextFile(workspace, "samplejson-1.txt");
 		//SampleDataFactory.createJsonWithFunnyCharacters(workspace);
-	//	SampleDataFactory.createSampleJson(workspace, 3);
+		SampleDataFactory.createSampleJson(workspace, 3);
 		//SampleDataFactory.createSampleJsonWithEmptyNestedTable1(workspace);
 		//SampleDataFactory.createSampleJsonWithEmptyNestedTable2(workspace);
 		//.createSampleJsonWithEmptyNestedTable3(workspace);
 		//SampleDataFactory.createSampleJsonWithEmptyNestedTable4(workspace);
+		SampleDataFactory.createFromJsonTextFile(workspace, "unit-test-json.json");
 		
 		// Put all created worksheet models in the view.
 		vwsp.addAllWorksheets();
