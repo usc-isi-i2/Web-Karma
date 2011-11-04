fd95b94b3e0598bfe195ab2ab696c69cd46fd274
Mohsen Taheriyan <mohsen@isi.edu>
2011-11-04 13:35:24 -0700

    Updating semantictypes/SemanticType.java to latest revision ...

--- src/main/java/edu/isi/karma/rep/semantictypes/SemanticType.java
+++ src/main/java/edu/isi/karma/rep/semantictypes/SemanticType.java
@@ -5,28 +5,47 @@ import org.json.JSONWriter;
 
 import edu.isi.karma.util.Jsonizable;
 
-public class SemanticType implements Jsonizable{
+public class SemanticType implements Jsonizable {
 	private final String hNodeId;
 	private final String type;
 	private final String domain;
 	private final Origin origin;
-	
+	private final ConfidenceLevel confidenceLevel;
+
 	public enum Origin {
 		User, CRFModel
 	}
-	
-	public SemanticType(String hNodeId, String type, Origin origin) {
+
+	public enum ConfidenceLevel {
+		High, Medium, Low
+	}
+
+	public SemanticType(String hNodeId, String type, Origin origin, Double probability) {
 		this.hNodeId = hNodeId;
 		this.type = type;
 		this.origin = origin;
 		this.domain = "";
+		
+		if(probability > 0.8)
+			confidenceLevel = ConfidenceLevel.High;
+		else if (probability < 0.8 && probability > 0.4)
+			confidenceLevel = ConfidenceLevel.Medium;
+		else
+			confidenceLevel = ConfidenceLevel.Low;
 	}
-
-	public SemanticType(String hNodeId, String type, String domain, Origin origin) {
+	
+	public SemanticType(String hNodeId, String type, String domain, Origin origin, Double probability) {
 		this.hNodeId = hNodeId;
 		this.type = type;
 		this.origin = origin;
 		this.domain = domain;
+		
+		if(probability > 0.8)
+			confidenceLevel = ConfidenceLevel.High;
+		else if (probability < 0.8 && probability > 0.4)
+			confidenceLevel = ConfidenceLevel.Medium;
+		else
+			confidenceLevel = ConfidenceLevel.Low;
 	}
 
 	public String getHNodeId() {
@@ -36,13 +55,13 @@ public class SemanticType implements Jsonizable{
 	public String getType() {
 		return type;
 	}
-	
-	public String getDomain() {
-		return domain;
-	}
-	
+
 	public Origin getOrigin() {
 		return origin;
+	}
+
+	public ConfidenceLevel getConfidenceLevel() {
+		return confidenceLevel;
 	}
 
 	@Override
@@ -51,6 +70,7 @@ public class SemanticType implements Jsonizable{
 		writer.key("HNodeId").value(hNodeId);
 		writer.key("Type").value(type);
 		writer.key("Origin").value(origin.name());
+		writer.key("ConfidenceLevel").value(confidenceLevel.name());
 		writer.endObject();
 	}
 }
