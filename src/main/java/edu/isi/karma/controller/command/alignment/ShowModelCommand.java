110afa954ef26e6fc29f037b1f4c39d1fe2474aa
Shubham Gupta <shuby.gupta@gmail.com>
2011-12-23 09:24:52 -0800

    Write source description to a file when doing alignment. Currently it outputs them into the log folder.

--- src/main/java/edu/isi/karma/controller/command/alignment/ShowModelCommand.java
+++ src/main/java/edu/isi/karma/controller/command/alignment/ShowModelCommand.java
@@ -1,5 +1,10 @@
 package edu.isi.karma.controller.command.alignment;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.isi.karma.controller.command.CommandException;
 import edu.isi.karma.controller.command.WorksheetCommand;
 import edu.isi.karma.controller.update.SemanticTypesUpdate;
@@ -8,11 +13,14 @@ import edu.isi.karma.modeling.alignment.AlignToOntology;
 import edu.isi.karma.modeling.semantictypes.SemanticTypeUtil;
 import edu.isi.karma.rep.Worksheet;
 import edu.isi.karma.view.VWorkspace;
+import edu.isi.karma.webserver.KarmaException;
 
 public class ShowModelCommand extends WorksheetCommand {
 
 	private final String vWorksheetId;
 	private String worksheetName;
+	
+	private static Logger logger = LoggerFactory.getLogger(ShowModelCommand.class);
 
 	protected ShowModelCommand(String id, String worksheetId,
 			String vWorksheetId) {
@@ -55,7 +63,14 @@ public class ShowModelCommand extends WorksheetCommand {
 		// Get the alignment update if any
 		AlignToOntology align = new AlignToOntology(worksheet, vWorkspace,
 				vWorksheetId);
-		align.update(c, semanticTypesChangedOrAdded);
+		try {
+			align.update(c, semanticTypesChangedOrAdded);
+		} catch (KarmaException e) {
+			logger.error("Error generating source description.", e);
+		} catch (IOException e) {
+			logger.error("Error writing source description file.", e);
+		}
+		
 
 		return c;
 	}
