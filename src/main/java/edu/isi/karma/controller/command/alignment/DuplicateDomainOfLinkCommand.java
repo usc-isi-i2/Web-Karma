42bda758d0a018a89dc63f9df86de8895602985a
Shubham Gupta <shuby.gupta@gmail.com>
2012-04-06 09:21:36 -0700

    Bug fix in the duplicate domain command

--- src/main/java/edu/isi/karma/controller/command/alignment/DuplicateDomainOfLinkCommand.java
+++ src/main/java/edu/isi/karma/controller/command/alignment/DuplicateDomainOfLinkCommand.java
@@ -37,7 +37,6 @@ import edu.isi.karma.modeling.alignment.LabeledWeightedEdge;
 import edu.isi.karma.modeling.alignment.Vertex;
 import edu.isi.karma.rdf.WorksheetRDFGenerator;
 import edu.isi.karma.rep.HNode;
-import edu.isi.karma.rep.HNodePath;
 import edu.isi.karma.rep.Worksheet;
 import edu.isi.karma.view.VWorksheet;
 import edu.isi.karma.view.VWorkspace;
@@ -101,10 +100,12 @@ public class DuplicateDomainOfLinkCommand extends Command {
 				.getSteinerTree();
 		Vertex root = alignment.GetTreeRoot();
 
-		List<HNode> sortedHeaders = worksheet.getHeaders().getSortedHNodes();
+		List<HNode> sortedHeaderNodes = new ArrayList<HNode>(); 
+		worksheet.getHeaders().getSortedLeafHNodes(sortedHeaderNodes);
+		
 		// Convert the tree into a AlignmentForest
 		AlignmentForest forest = AlignmentForest.constructFromSteinerTree(tree,
-				root, sortedHeaders);
+				root, sortedHeaderNodes);
 		AlignmentHeadersUpdate alignmentUpdate = new AlignmentHeadersUpdate(
 				forest, vWorksheetId, alignmentId);
 		GraphUtil.printGraph(tree);
@@ -119,13 +120,13 @@ public class DuplicateDomainOfLinkCommand extends Command {
 
 		
 		// Create new vWorksheet using the new header order
-		List<HNodePath> columnPaths = new ArrayList<HNodePath>();
-		for (HNode node : sortedHeaders) {
-			HNodePath path = new HNodePath(node);
-			columnPaths.add(path);
-		}
-		vWorkspace.getViewFactory().updateWorksheet(vWorksheetId, worksheet,
-				columnPaths, vWorkspace);
+//		List<HNodePath> columnPaths = new ArrayList<HNodePath>();
+//		for (HNode node : sortedHeaders) {
+//			HNodePath path = new HNodePath(node);
+//			columnPaths.add(path);
+//		}
+//		vWorkspace.getViewFactory().updateWorksheet(vWorksheetId, worksheet,
+//				columnPaths, vWorkspace);
 		VWorksheet vw = vWorkspace.getViewFactory().getVWorksheet(vWorksheetId);
 
 		UpdateContainer c = new UpdateContainer();
