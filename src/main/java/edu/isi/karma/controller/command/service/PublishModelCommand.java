5ef85b26e9bbefe07213bde96fdab787199ad17c
mohsen <mohsen@isi.edu>
2012-08-16 13:33:41 -0700

    eliminate a warning in PublishModelCommand.java

--- src/main/java/edu/isi/karma/controller/command/service/PublishModelCommand.java
+++ src/main/java/edu/isi/karma/controller/command/service/PublishModelCommand.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.util.List;
 
 import org.jgrapht.graph.DirectedWeightedMultigraph;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
