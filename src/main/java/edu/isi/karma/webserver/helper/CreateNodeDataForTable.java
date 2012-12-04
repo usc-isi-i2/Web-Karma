9d0ae2aa0696a6203f1ec66ca88a1ba908b8baf5
dearpp <dearzppzpp@163.com>
2012-12-04 11:08:00 -0800

    fix the warning bugs
    fix the warning bugs

--- src/main/java/edu/isi/karma/webserver/helper/CreateNodeDataForTable.java
+++ src/main/java/edu/isi/karma/webserver/helper/CreateNodeDataForTable.java
@@ -2,11 +2,13 @@ package edu.isi.karma.webserver.helper;
 
 import java.io.File;
 import java.sql.Connection;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
 
 import org.dom4j.Attribute;
 import org.dom4j.Document;
@@ -19,7 +21,6 @@ public class CreateNodeDataForTable {
 	private Connection connection = null;
 	private String osmFile_path;
 	private Statement stmt = null;
-	private ResultSet rs = null;
 
 	public CreateNodeDataForTable(Connection connection, String osmFile_path) {
 		this.connection = connection;
@@ -32,58 +33,70 @@ public class CreateNodeDataForTable {
 
 	}
 
+	private <T> List<T> castList(Class<T> clazz, Collection<?> c) {
+		List<T> list = new Vector<T>(c.size());
+		for (Object object : c) {
+			System.out.println(object.getClass());
+			list.add(clazz.cast(object));
+		}
+		return list;
+	}
+
+	private <T> List<T> castIterator(Class<T> clazz, Iterator<?> i) {
+		List<T> list = new Vector<T>();
+		while (i.hasNext()) {
+			list.add(clazz.cast(i.next()));
+		}
+		return list;
+	}
+
 	/*
 	 * Create the table for nodes
 	 */
 	public void createNodeDataforTable() {
 
 		try {
-			rs = stmt.executeQuery("drop TABLE postgis.public.nodestable");
+			stmt.executeQuery("drop TABLE postgis.public.nodestable");
 		} catch (SQLException ee) {
 			ee.getStackTrace();
 		}
 		try {
-			rs = stmt
-					.executeQuery("CREATE TABLE postgis.public.nodestable (ord integer PRIMARY KEY, id integer, lat double precision, lon double precision, uid integer, visible boolean, version integer, changeset integer, users character varying, timestamp timestamp without time zone)");
+			stmt.executeQuery("CREATE TABLE postgis.public.nodestable (ord integer PRIMARY KEY, id integer, lat double precision, lon double precision, uid integer, visible boolean, version integer, changeset integer, users character varying, timestamp timestamp without time zone)");
 		} catch (SQLException ee) {
 			ee.getStackTrace();
 		}
 
-
 		try {
-			rs = stmt.executeQuery("drop TABLE postgis.public.nodes_tag");
+			stmt.executeQuery("drop TABLE postgis.public.nodes_tag");
 		} catch (SQLException ee) {
 			ee.getStackTrace();
 		}
 		try {
-			rs = stmt
-					.executeQuery("CREATE TABLE postgis.public.nodes_tag (number integer PRIMARY KEY, node_id integer, tag_order integer, k character varying, v character varying)");
+			stmt.executeQuery("CREATE TABLE postgis.public.nodes_tag (number integer PRIMARY KEY, node_id integer, tag_order integer, k character varying, v character varying)");
 		} catch (SQLException ee) {
 			ee.getStackTrace();
 		}
 
-	    
 		int ord = 1;
 		int columofnodes_tag = 1;
 		SAXReader saxReadering = new SAXReader();
 		try {
 			Document document = saxReadering.read(new File(this.osmFile_path));
-			List list = document.selectNodes("//osm/node");
-			Iterator iter = list.iterator();
 			String node_id = null;
-			while (iter.hasNext()) {
-				Element ele = (Element) iter.next();
-				String ss = ele.getName();
+			List<Element> list = castList(Element.class,
+					document.selectNodes("//osm/node"));
+			for (Element ele : list) {
 				try {
-					rs = stmt
-							.executeQuery("insert into postgis.public.nodestable (ord) values ("
-									+ ord + ")");
+
+					stmt.executeQuery("insert into postgis.public.nodestable (ord) values ("
+							+ ord + ")");
 				} catch (SQLException ee) {
 					ee.getStackTrace();
 				}
 
-				for (Iterator ite = ele.attributeIterator(); ite.hasNext();) {
-					Attribute attribute = (Attribute) ite.next();
+				List<Attribute> ite = castIterator(Attribute.class,
+						ele.attributeIterator());
+				for (Attribute attribute : ite) {
 					String name = attribute.getName();
 					String value = attribute.getText();
 					if (name.equals("user")) {
@@ -92,65 +105,53 @@ public class CreateNodeDataForTable {
 						node_id = value;
 					}
 					try {
-						rs = stmt
-								.executeQuery("update postgis.public.nodestable set "
-										+ name
-										+ "=\'"
-										+ value
-										+ "\' where ord=" + ord);
+						stmt.executeQuery("update postgis.public.nodestable set "
+								+ name + "=\'" + value + "\' where ord=" + ord);
+
 					} catch (SQLException ee) {
 						ee.getStackTrace();
 					}
 
 				}
 
-
-				List nods = ele.elements("tag");
 				int colm_tag = 1;
-				for (Iterator its = nods.iterator(); its.hasNext();) {
-
+				List<Element> nods = castList(Element.class,
+						ele.elements("tag"));
+				for (Element elms : nods) {
 					try {
-						rs = stmt
-								.executeQuery("insert into postgis.public.nodes_tag (number) values ("
-										+ columofnodes_tag + ")");
+						stmt.executeQuery("insert into postgis.public.nodes_tag (number) values ("
+								+ columofnodes_tag + ")");
 					} catch (SQLException ee) {
 						ee.getStackTrace();
 					}
 
 					try {
-						rs = stmt
-								.executeQuery("update postgis.public.nodes_tag set node_id=\'"
-										+ node_id
-										+ "\'where number=\'"
-										+ columofnodes_tag + "\'");
+						stmt.executeQuery("update postgis.public.nodes_tag set node_id=\'"
+								+ node_id
+								+ "\', tag_order=\'"
+								+ colm_tag
+								+ "\' where number=\'"
+								+ columofnodes_tag
+								+ "\'");
 					} catch (SQLException ee) {
 						ee.getStackTrace();
 					}
 
-					try {
-						rs = stmt
-								.executeQuery("update postgis.public.nodes_tag set tag_order=\'"
-										+ colm_tag
-										+ "\'where number=\'"
-										+ columofnodes_tag + "\'");
-					} catch (SQLException ee) {
-						ee.getStackTrace();
-					}
-
-					Element elms = (Element) its.next();
-					for (Iterator iters = elms.attributeIterator(); iters.hasNext();) {
+					List<Attribute> itLists = castIterator(Attribute.class,
+							elms.attributeIterator());
+					for (Iterator<Attribute> iters = itLists.iterator(); iters
+							.hasNext();) {
 						Attribute attribute = (Attribute) iters.next();
 						String name = attribute.getName();
 						String value = attribute.getText();
-
 						try {
-							rs = stmt
-									.executeQuery("update postgis.public.nodes_tag set "
-											+ name
-											+ "=\'"
-											+ value
-											+ "\' where number=\'"
-											+ columofnodes_tag + "\'");
+							stmt.executeQuery("update postgis.public.nodes_tag set "
+									+ name
+									+ "=\'"
+									+ value
+									+ "\' where number=\'"
+									+ columofnodes_tag
+									+ "\'");
 						} catch (SQLException ee) {
 							ee.getStackTrace();
 						}
