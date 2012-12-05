2373bdb846e66488ebcf7f771c70307e090252ac
mohsen taheriyan <mohsen@isi.edu>
2012-12-05 09:35:04 -0800

    wrong URL fixed.

--- src/main/java/edu/isi/karma/service/Namespaces.java
+++ src/main/java/edu/isi/karma/service/Namespaces.java
@@ -33,6 +33,6 @@ public interface Namespaces {
 	public static final String HRESTS = "http://purl.org/hRESTS/current#";
 	public static final String RULEML = "http://www.w3.org/2003/11/ruleml#";
 	public static final String SWRL = "http://www.w3.org/2003/11/swrl#";
-	public static final String KARMA = "http://isi.edu/integration/karma/model/current#";
+	public static final String KARMA = "http://isi.edu/integration/karma/ontologies/model/current#";
 
 }
