d91d53ee4d18fb2d65fdb890150a0e051066b961
Shrikanth Narayanan <nara471@usc.edu>
2014-03-10 01:59:37 -0700

    Modified old implementation for InvokeDataMiningServiceCommand Now using ExportCSVUtil to generate the csv for the columns and then posting the csv file as POST payload to the data mining service

--- src/main/java/edu/isi/karma/controller/command/alignment/InvokeDataMiningServiceCommand.java
+++ src/main/java/edu/isi/karma/controller/command/alignment/InvokeDataMiningServiceCommand.java
@@ -21,9 +21,25 @@
 
 package edu.isi.karma.controller.command.alignment;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +48,8 @@ import edu.isi.karma.controller.command.Command;
 import edu.isi.karma.controller.command.CommandException;
 import edu.isi.karma.controller.command.publish.PublishRDFCommand;
 import edu.isi.karma.controller.command.publish.PublishRDFCommand.PreferencesKeys;
+import edu.isi.karma.controller.command.worksheet.ExportCSVCommand.JsonKeys;
+import edu.isi.karma.controller.update.AbstractUpdate;
 import edu.isi.karma.controller.update.ErrorUpdate;
 import edu.isi.karma.controller.update.InvokeDataMiningServiceUpdate;
 import edu.isi.karma.controller.update.UpdateContainer;
@@ -46,14 +64,22 @@ import edu.isi.karma.modeling.alignment.AlignmentManager;
 import edu.isi.karma.rep.Worksheet;
 import edu.isi.karma.rep.Workspace;
 import edu.isi.karma.util.HTTPUtil;
+import edu.isi.karma.util.HTTPUtil.HTTP_HEADERS;
+import edu.isi.karma.view.VWorkspace;
+import edu.isi.karma.webserver.ServletContextParameterMap;
+import edu.isi.karma.webserver.ServletContextParameterMap.ContextParameter;
 
 public class InvokeDataMiningServiceCommand extends Command {
 	private static Logger logger = LoggerFactory.getLogger(InvokeDataMiningServiceCommand.class);
 	private final String worksheetId;
 	
-	private String tripleStoreUrl;
-	private String modelContext;
+//	private String tripleStoreUrl;
+	private final String csvFileName;
+//	private String graphUrl;
 	private String dataMiningURL;
+//	private String generatedRDFFileName = null;
+//	private final ArrayList<String> columnList;
+//	private final String rootNodeId;
 	
 	public String getDataMiningURL() {
 		return dataMiningURL;
@@ -63,32 +89,47 @@ public class InvokeDataMiningServiceCommand extends Command {
 		this.dataMiningURL = dataMiningURL;
 	}
 
-	public String getModelContext() {
-		return modelContext;
-	}
+//	public String getTripleStoreUrl() {
+//		return tripleStoreUrl;
+//	}
+//
+//	public void setTripleStoreUrl(String tripleStoreUrl) {
+//		this.tripleStoreUrl = tripleStoreUrl;
+//	}
 
-	public void setModelContext(String modelContext) {
-		this.modelContext = modelContext;
-	}
-
-	public String getTripleStoreUrl() {
-		return tripleStoreUrl;
-	}
-
-	public void setTripleStoreUrl(String tripleStoreUrl) {
-		this.tripleStoreUrl = tripleStoreUrl;
-	}
-
-	protected InvokeDataMiningServiceCommand(String id, String worksheetId, String url, String graph, String miningUrl) {
+	/**
+	 * @param id
+	 * @param worksheetId
+	 * @param miningUrl
+	 * @param csvFileName
+	 * */
+	protected InvokeDataMiningServiceCommand(String id, String worksheetId, String miningUrl, String fileName) {
 		super(id);
 		this.worksheetId = worksheetId;
-		if (url == null || url.isEmpty()) {
-			url = TripleStoreUtil.defaultDataRepoUrl;
-		}
-		this.tripleStoreUrl = url;
-		this.modelContext = graph;
 		this.dataMiningURL = miningUrl;
+		this.csvFileName = fileName;
 	}
+	/**
+	 * @param id
+	 * @param worksheetId
+	 * @param rootNode
+	 * @param sparqlUrl
+	 * @param graph
+	 * @param miningUrl
+	 * @param nodes
+	 * */
+//	protected InvokeDataMiningServiceCommand(String id, String worksheetId, String rootNode, String sparqlUrl, String graph, String miningUrl, ArrayList<String> nodes) {
+//		super(id);
+//		this.worksheetId = worksheetId;
+//		if (sparqlUrl == null || sparqlUrl.isEmpty()) {
+//			sparqlUrl = TripleStoreUtil.defaultDataRepoUrl;
+//		}
+//		this.tripleStoreUrl = sparqlUrl;
+//		this.graphUrl = graph;
+//		this.dataMiningURL = miningUrl;
+//		this.rootNodeId = rootNode;
+//		this.columnList = nodes;
+//	}
 
 	@Override
 	public String getCommandName() {
@@ -152,62 +193,101 @@ public class InvokeDataMiningServiceCommand extends Command {
 //	}
 	
 	@Override
-	public UpdateContainer doIt(Workspace workspace) throws CommandException {
+	public UpdateContainer doIt(Workspace workspace) {
+		
+		final String csvFileLocalPath = ServletContextParameterMap.getParameterValue(
+				ContextParameter.USER_DIRECTORY_PATH) +  "publish/CSV/" + this.csvFileName;
 		
 //		String jsonString = fetch_data_temp();
 		try {
 			
 			// Get the alignment for this Worksheet
-			Alignment alignment = AlignmentManager.Instance().getAlignment(AlignmentManager.
-					Instance().constructAlignmentId(workspace.getId(), worksheetId));
-			
-			if (alignment == null) {
-				logger.info("Alignment is NULL for " + worksheetId);
-				return new UpdateContainer(new ErrorUpdate(
-						"Please align the worksheet before generating R2RML Model!"));
-			}
-			// Get the namespace and prefix from the preferences
-			String namespace = "";
-			String prefix = "";
-			JSONObject prefObject = workspace.getCommandPreferences().getCommandPreferencesJSONObject(
-					PublishRDFCommand.class.getSimpleName()+"Preferences");
-			if (prefObject != null) {
-				namespace = prefObject.getString(PreferencesKeys.rdfNamespace.name());
-				prefix = prefObject.getString(PreferencesKeys.rdfPrefix.name());
-				namespace = ((namespace == null) || (namespace.equals(""))) ? 
-						Namespaces.KARMA_DEV : namespace;
-				prefix = ((prefix == null) || (prefix.equals(""))) ? 
-						Prefixes.KARMA_DEV : prefix;
-			} else {
-				namespace = Namespaces.KARMA_DEV;
-				prefix = Prefixes.KARMA_DEV;
-			}
-
-			Worksheet worksheet = workspace.getWorksheet(worksheetId);
-			// Generate the KR2RML data structures for the RDF generation
-			final ErrorReport errorReport = new ErrorReport();
-			KR2RMLMappingGenerator mappingGen = new KR2RMLMappingGenerator(workspace, worksheet, alignment, 
-					worksheet.getSemanticTypes(), prefix, namespace, true, errorReport);
-			
-			SPARQLGeneratorUtil genObj = new SPARQLGeneratorUtil();
-			String query = genObj.get_query(mappingGen.getKR2RMLMapping(), this.modelContext);
-			
-			// execute the query on the triple store
-			String data = TripleStoreUtil.invokeSparqlQuery(query, tripleStoreUrl, "application/sparql-results+json", null);
-
-			// prepare the input for the data mining service
-//			int row_num = 0;
-			
-			logger.debug(data);
-			
+//			Alignment alignment = AlignmentManager.Instance().getAlignment(AlignmentManager.
+//					Instance().constructAlignmentId(workspace.getId(), worksheetId));
+//			
+//			if (alignment == null) {
+//				logger.info("Alignment is NULL for " + worksheetId);
+//				return new UpdateContainer(new ErrorUpdate(
+//						"Please align the worksheet before generating R2RML Model!"));
+//			}
+//			// Get the namespace and prefix from the preferences
+//			String namespace = "";
+//			String prefix = "";
+//			JSONObject prefObject = workspace.getCommandPreferences().getCommandPreferencesJSONObject(
+//					PublishRDFCommand.class.getSimpleName()+"Preferences");
+//			if (prefObject != null) {
+//				namespace = prefObject.getString(PreferencesKeys.rdfNamespace.name());
+//				prefix = prefObject.getString(PreferencesKeys.rdfPrefix.name());
+//				namespace = ((namespace == null) || (namespace.equals(""))) ? 
+//						Namespaces.KARMA_DEV : namespace;
+//				prefix = ((prefix == null) || (prefix.equals(""))) ? 
+//						Prefixes.KARMA_DEV : prefix;
+//			} else {
+//				namespace = Namespaces.KARMA_DEV;
+//				prefix = Prefixes.KARMA_DEV;
+//			}
+//
+//			Worksheet worksheet = workspace.getWorksheet(worksheetId);
+//			// Generate the KR2RML data structures for the RDF generation
+//			final ErrorReport errorReport = new ErrorReport();
+//			KR2RMLMappingGenerator mappingGen = new KR2RMLMappingGenerator(workspace, worksheet, alignment, 
+//					worksheet.getSemanticTypes(), prefix, namespace, true, errorReport);
+//			
+//			SPARQLGeneratorUtil genObj = new SPARQLGeneratorUtil();
+//			String query = genObj.get_query(mappingGen.getKR2RMLMapping(), this.modelContext);
+//			
+//			// execute the query on the triple store
+//			String data = TripleStoreUtil.invokeSparqlQuery(query, tripleStoreUrl, "application/sparql-results+json", null);
+//
+//			// prepare the input for the data mining service
+////			int row_num = 0;
+//			
+//			logger.debug(data);
+//			
 			// post the results 
 			//TODO : integrate the service with karma
-			Map<String, String> formParameters = new HashMap<String, String>();
-			formParameters.put("data", data);
-			String response = HTTPUtil.executeHTTPPostRequest("http://localhost:1234/consumejson", null, null, formParameters);
-			return new UpdateContainer(new InvokeDataMiningServiceUpdate(new JSONObject().put("data", response), 
-					InvokeDataMiningServiceUpdate.DataPrcessingFormats.testFormat.name()));
 			
+			// Prepare the headers
+			HttpPost httpPost = new HttpPost(this.dataMiningURL);
+	
+			FileEntity file = new FileEntity(new File(csvFileLocalPath));
+			httpPost.setEntity(file);
+			HttpClient httpClient = new DefaultHttpClient();
+			httpPost.setHeader("Content-Type", "text/csv");
+			
+			// Execute the request
+			HttpResponse response = httpClient.execute(httpPost);
+			
+			// Parse the response and store it in a String
+			HttpEntity entity = response.getEntity();
+			StringBuilder responseString = new StringBuilder();
+			if (entity != null) {
+				BufferedReader buf = new BufferedReader(new InputStreamReader(entity.getContent(),"UTF-8"));
+				
+				String line = buf.readLine();
+				while(line != null) {
+					responseString.append(line);
+					line = buf.readLine();
+				}
+			}
+			logger.info(responseString.toString());
+			final String modelFileName = responseString.toString(); 
+			UpdateContainer uc = new UpdateContainer();
+			uc.add(new AbstractUpdate() {
+				public void generateJson(String prefix, PrintWriter pw,	
+						VWorkspace vWorkspace) {
+					JSONObject outputObject = new JSONObject();
+					try {
+						outputObject.put("updateType", "InvokeDataMiningServiceUpdate");
+						outputObject.put("model_name", modelFileName);
+						pw.println(outputObject.toString());
+					} catch (JSONException e) {
+						logger.error("Error occured while generating JSON!");
+					}
+				}
+			});
+			return uc;
+					
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			return new UpdateContainer(new ErrorUpdate("Error !"));
