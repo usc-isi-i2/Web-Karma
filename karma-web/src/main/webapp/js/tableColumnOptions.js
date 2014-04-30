dfe9cdedf02a87d59b0c39c0b485b8d71fc8ebcd
Dipsy Kapoor <dipsykapoor@gmail.com>
2014-04-30 09:57:54 -0700

    Fix for WK98: Invalid commands in nested tables

--- karma-web/src/main/webapp/js/tableColumnOptions.js
+++ karma-web/src/main/webapp/js/tableColumnOptions.js
@@ -1,31 +1,30 @@
-function TableColumnOptions(wsId, wsColumnId, wsColumnTitle) {
+function TableColumnOptions(wsId, wsColumnId, wsColumnTitle, isLeafNode) {
 	 
 	var worksheetId = wsId;
 	var columnTitle = wsColumnTitle;
 	var columnId = wsColumnId;
 	
 	var options = [
-	        // Title, function to call, needs file upload
-	        [ "Set Semantic Type", setSemanticType],
-	        [ "divider", null],
+	               {name:"Set Semantic Type", func:setSemanticType, leafOnly:true},
+	               {name:"divider", leafOnly:true},
 
-	        [	"Add Column" , addColumn ],
-			[ "Rename", renameColumn ],
-			[ "Split Column", splitColumn ],
-			[ "Add Row", addRow],
-			[ "divider" , null ],
+	               {name:"Add Column", func:addColumn, leafOnly:false},
+	               {name:"Rename", func:renameColumn, leafOnly:true},
+	               {name:"Split Column", func:splitColumn, leafOnly:true},
+	               {name:"Add Row", func:addRow, leafOnly:false},
+	               {name:"divider" , leafOnly:false},
 			
-			["Extract Entities",extractEntities],
-			[ "PyTransform" , pyTransform ],
-			[ "Transform", transform],
-			[ "divider" , null ],
+	               {name:"Extract Entities", func:extractEntities, leafOnly:true},
+	               {name:"PyTransform" , func:pyTransform, leafOnly:true},
+	               {name:"Transform", func:transform, leafOnly:true},
+	               {name:"divider" , leafOnly:true},
 			
-			[ "Invoke Service" , invokeService ],
-			[ "Show Chart", showChart],
-
-			[ "divider" , null ],
-			["Group By", GroupBy], 
-			["Unfold", Unfold] 
+	               {name:"Invoke Service" , func:invokeService, leafOnly:true},
+	               {name:"Show Chart", func:showChart, leafOnly:true},
+	               {name:"divider" , leafOnly:true},
+	               
+	               {name:"Group By", func:GroupBy, leafOnly:false},
+	               {name:"Unfold", func:Unfold, leafOnly:false},
 	];
 	
 	function hideDropdown() {
@@ -155,50 +154,56 @@ function TableColumnOptions(wsId, wsColumnId, wsColumnTitle) {
 	
 	this.generateJS = function() {
 		var dropdownId = "columnOptionsButton" + worksheetId + "_" + columnId;
+		var span = $("<span>")
+						.attr("display", "inline-block")
+						.addClass("tableDropdown")
+						.addClass("dropdown")
+						.append($("<a>")
+							.attr("href", "#")
+							.addClass("dropdown-toggle")
+							.addClass("ColumnTitle")
+							.attr("id", dropdownId)
+							.data("worksheetId", worksheetId)
+							.attr("data-toggle", "dropdown")
+							.text(columnTitle)
+							.append($("<span>").addClass("caret")
+							)
+						);
+		
 		var div = 
 			$("<div>")
 				.attr("id", "TableOptionsDiv")
 				.data("worksheetId", worksheetId)
-				.addClass("tableDropdown")
-				.addClass("dropdown")
-				.append($("<a>")
-						.attr("href", "#")
-						.addClass("dropdown-toggle")
-						.addClass("ColumnTitle")
-						.attr("id", dropdownId)
-						.data("worksheetId", worksheetId)
-						.attr("data-toggle", "dropdown")
-						.text(columnTitle)
-						.append($("<span>").addClass("caret")
-						)
-				);
-                
-
+				.append(span)
+				;     
+		
 		var ul = $("<ul>").addClass("dropdown-menu");
 		ul.attr("role", "menu")
 			.attr("aria-labelledby", dropdownId);
 		// console.log("There are " + options.length + " menu items");
 		for(var i=0; i<options.length; i++) {
 			var option = options[i];
-			var needFile = false;
-			if(option.length > 2 && option[2] == true)
-				needFile = true;
+			
+			if(option.leafOnly ==true && isLeafNode == false) {
+				continue;
+			}
+			
+			var needFile = option.useFileUpload;
+			
 			var li = $("<li>");
-			// console.log("Got option" + option);
-			var title = option[0];
+			//console.log("Got option" +  option);
+			var title = option.name;
 			if(title == "divider")
 				li.addClass("divider");
 			else {
-				var func = option[1];
+				var func = option.func;
 				var a = $("<a>")
 						.attr("href", "#");
 				if(needFile) {
-					// <form id="fileupload" action="ImportFileCommand"
-					// method="POST" enctype="multipart/form-data">From
-					// File<input type="file" name="files[]" multiple></form>
+					//<form id="fileupload" action="ImportFileCommand" method="POST" enctype="multipart/form-data">From File<input type="file" name="files[]" multiple></form>
 					a.addClass("fileinput-button");
 					var form = $("<form>")
-								.attr("id", option[3])
+								.attr("id", option.uploadDiv + "_" + worksheetId)
 								.attr("action", "ImportFileCommand")
 								.attr("method", "POST")
 								.attr("enctype", "multipart/form-data")
@@ -210,14 +215,26 @@ function TableColumnOptions(wsId, wsColumnId, wsColumnTitle) {
 					a.append(form);
 					window.setTimeout(func, 1000);
 				} else {
-					a.text(title);
-					a.click(func);
+					if(option.showCheckbox) {
+						var checkbox = $("<input>").attr("type", "checkbox");
+						if(option.defaultChecked)
+							checkbox.attr("checked","checked");
+						var label = $("<span>").append(checkbox).append("&nbsp;").append(title);
+						a.append(label);
+						a.click(func);
+					} else {
+						a.text(title);
+						a.click(func);
+					}
+					
 				}
 				li.append(a);
 			}
+			if(option.initFunc)
+				option.initFunc();
 			ul.append(li);
 		};
-		div.append(ul);
+		span.append(ul);
 		return div;
 	};
 };
