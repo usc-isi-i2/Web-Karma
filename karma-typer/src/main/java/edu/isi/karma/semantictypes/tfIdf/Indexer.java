657ccc412aad50d4f4a195e95631cc6120918f60
Dipsy Kapoor <dipsykapoor@gmail.com>
2014-07-16 12:52:30 -0700

    remove unused semantic type file. Format typer files

--- karma-typer/src/main/java/edu/isi/karma/semantictypes/tfIdf/Indexer.java
+++ karma-typer/src/main/java/edu/isi/karma/semantictypes/tfIdf/Indexer.java
@@ -17,66 +17,73 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 /**
- * This class is responsible for creation of an index, adding documents to the index,
- * committing changes
+ * This class is responsible for creation of an index, adding documents to the
+ * index, committing changes
  * 
  * @author ramnandan
- *
+ * 
  */
 
 public class Indexer {
 	public static String CONTENT_FIELD_NAME = "content";
 	public static String LABEL_FIELD_NAME = "label";
+
+	private IndexWriter indexWriter = null;
+	private Directory indexDirectory;
+
+	public Indexer(String filepath) throws IOException {
+		indexDirectory = FSDirectory.open(new File(filepath));
+	}
+
+	public void open() throws IOException {
+
+		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_48);
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_48,
+				analyzer);
+
+		// Creates a new index if one does not exist,
+		// otherwise opens the index and documents will be appended
+		config.setOpenMode(OpenMode.CREATE_OR_APPEND);
+
+		indexWriter = new IndexWriter(indexDirectory, config);
+	}
+
+	public void commit() throws IOException {
+		indexWriter.commit();
+	}
 	
-  private IndexWriter indexWriter = null;
-  
-  public void openIndexWriter(String filepath) throws IOException
-  {
-  	Directory dir = FSDirectory.open(new File(filepath));
-  	Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_48); 
-  	IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_48, analyzer);
-  	
-  	// Creates a new index if one does not exist, 
-  	// otherwise opens the index and documents will be appended
-  	config.setOpenMode(OpenMode.CREATE_OR_APPEND);
-  	
-  	indexWriter = new IndexWriter(dir, config);
-  }   
-  
-  public void closeIndexWriter() throws IOException
-  {
-  	indexWriter.close();
-  }
-  
-  public void addDocument(String content, String label) throws IOException 
-  {
-  	Document doc = new Document();
-  	doc.add(new TextField(CONTENT_FIELD_NAME, content, Field.Store.YES));
-  	doc.add(new StringField(LABEL_FIELD_NAME, label, Field.Store.YES));
-  	indexWriter.addDocument(doc);
-  }
-  
-  public void updateDocument(String content, String label) throws IOException 
-  {
-  	Document doc = new Document();
-  	doc.add(new TextField(CONTENT_FIELD_NAME, content, Field.Store.YES));
-  	doc.add(new StringField(LABEL_FIELD_NAME, label, Field.Store.YES));
-  	indexWriter.updateDocument(new Term(LABEL_FIELD_NAME,label), doc);
-  }
-  
-  public void deleteDocuments() throws IOException
-  {
-  	indexWriter.deleteAll();
-  }
-  
-  public int getNoOfDocs()
-  {
-  	return indexWriter.numDocs();
-  }
-  
-  public void commit() throws IOException
-  {
-  	indexWriter.commit();
-  }
-  
+	public void close() throws IOException {
+		try {
+			indexWriter.close(false);
+		} finally {
+			if (IndexWriter.isLocked(indexDirectory)) {
+				IndexWriter.unlock(indexDirectory);
+			}
+		}
+	}
+
+	public void addDocument(String content, String label) throws IOException {
+		Document doc = new Document();
+		doc.add(new TextField(CONTENT_FIELD_NAME, content, Field.Store.YES));
+		doc.add(new StringField(LABEL_FIELD_NAME, label, Field.Store.YES));
+		indexWriter.addDocument(doc);
+	}
+
+	public void updateDocument(String content, String label) throws IOException {
+		Document doc = new Document();
+		doc.add(new TextField(CONTENT_FIELD_NAME, content, Field.Store.YES));
+		doc.add(new StringField(LABEL_FIELD_NAME, label, Field.Store.YES));
+		indexWriter.updateDocument(new Term(LABEL_FIELD_NAME, label), doc);
+	}
+
+	public void deleteAllDocuments() throws IOException {
+		indexWriter.deleteAll();
+	}
+
+	public int getNumberOfDocuments() {
+		return indexWriter.numDocs();
+	}
+
+	
+
 }
