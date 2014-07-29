8a9e5d9d14652e525d6ec618767537d556f0f7b9
Jason Slepicka <slepicka@usc.edu>
2014-07-28 18:40:33 -0700

    Disable reloading of user python libraries offline

--- karma-commands/commands-python/src/main/java/edu/isi/karma/controller/command/transformation/PythonRepository.java
+++ karma-commands/commands-python/src/main/java/edu/isi/karma/controller/command/transformation/PythonRepository.java
@@ -18,14 +18,15 @@ public class PythonRepository {
 	private ConcurrentHashMap<String, PyCode> libraryScripts;
 	private ConcurrentHashMap<String, Long> fileNameTolastTimeRead;
 	private static PythonRepository instance = new PythonRepository();
+	private static boolean libraryHasBeenLoaded = false;
+	private static boolean reloadLibrary = true;
 	
 	private PythonRepository()
 	{
-		scripts = new ConcurrentHashMap<String, PyCode>();
-		libraryScripts = new ConcurrentHashMap<String, PyCode>();
-		fileNameTolastTimeRead = new ConcurrentHashMap<String,Long>();
 		initialize();
+		resetLibrary();
 	}
+	
 	public static PythonRepository getInstance()
 	{
 		return instance;
@@ -33,6 +34,7 @@ public class PythonRepository {
 	
 	private void initialize()
 	{
+		scripts = new ConcurrentHashMap<String, PyCode>();
 		PythonInterpreter interpreter = new PythonInterpreter();
 		compileAndAddToRepository(interpreter, PythonTransformationHelper.getImportStatements());
 		compileAndAddToRepository(interpreter, PythonTransformationHelper.getGetValueDefStatement());
@@ -51,11 +53,16 @@ public class PythonRepository {
 		PyCode py  = null;
 		if(!scripts.containsKey(statement))
 		{
-			py = interpreter.compile(statement);
+			py = compile(interpreter, statement);
 			scripts.putIfAbsent(statement, py);
 		}
 		return scripts.get(statement);
 	}
+	
+	private PyCode compile(PythonInterpreter interpreter, String statement) {
+		return interpreter.compile(statement);
+	}
+	
 	public void initializeInterperter(PythonInterpreter interpreter)
 	{
 		interpreter.exec(scripts.get(PythonTransformationHelper.getImportStatements()));
@@ -69,7 +76,7 @@ public class PythonRepository {
 		return scripts.get(PythonTransformationHelper.getTransformStatement());
 	}
 
-	public void importUserScripts(PythonInterpreter interpreter) throws IOException {
+	public synchronized void importUserScripts(PythonInterpreter interpreter) throws IOException {
 		String dirpathString = ServletContextParameterMap
 				.getParameterValue(ContextParameter.USER_PYTHON_SCRIPTS_DIRECTORY);
 
@@ -82,26 +89,52 @@ public class PythonRepository {
 				public boolean accept(File dir, String name) {
 					return name.endsWith(".py");
 				}});
-			for(String script : scripts)
+			if(!libraryHasBeenLoaded || reloadLibrary)
 			{
-				String fileName = dirpathString  + script;
-				Long lastTimeRead = fileNameTolastTimeRead.get(fileName);
-				File s = new File(fileName);
-				if(lastTimeRead == null || s.lastModified() > lastTimeRead)
+				for(String script : scripts)
 				{
-					String statement = FileUtils.readFileToString(s);
-					PyCode py = compileAndAddToRepository(interpreter, statement);
-					libraryScripts.put(fileName, py);
-					fileNameTolastTimeRead.put(fileName, System.currentTimeMillis());
+					String fileName = dirpathString  + script;
+					Long lastTimeRead = fileNameTolastTimeRead.get(fileName);
+					File s = new File(fileName);
+					if(lastTimeRead == null || s.lastModified() > lastTimeRead)
+					{
+						String statement = FileUtils.readFileToString(s);
+						PyCode py = compile(interpreter, statement);
+						libraryScripts.put(fileName, py);
+						fileNameTolastTimeRead.put(fileName, System.currentTimeMillis());
+					}
+					else
+					{
+						interpreter.exec(libraryScripts.get(fileName));
+					}
+					
+					//TODO prune scripts no longer present
 				}
-				else
-				{
-					interpreter.exec(libraryScripts.get(fileName));
-				}
+				libraryHasBeenLoaded = true;
 				
-				//TODO prune scripts no longer present
 			}
+			else
+			{
+				for(PyCode code : libraryScripts.values())
+				{
+					interpreter.exec(code);
+				}
+			}
+			
 		}
+		
+	}
+	
+	public static synchronized void disableReloadingLibrary()
+	{
+		reloadLibrary = false;
+	}
+	
+	public synchronized void resetLibrary()
+	{
+		libraryScripts = new ConcurrentHashMap<String, PyCode>();
+		fileNameTolastTimeRead = new ConcurrentHashMap<String,Long>();		
+		libraryHasBeenLoaded = false;
 		
 	}
 }
