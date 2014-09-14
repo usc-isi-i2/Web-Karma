5293644b1453302a4c389c1b60f347e2f94f0159
Jason Slepicka <slepicka@usc.edu>
2014-09-14 16:39:28 -0700

    Fix sql statements for Hive to handle tables that don't exist

--- karma-mr/src/main/resources/merge.sql
+++ karma-mr/src/main/resources/merge.sql
@@ -1,8 +1,8 @@
 CREATE TEMPORARY FUNCTION MergeJSON as 'edu.isi.karma.mapreduce.function.MergeJSON';
-INSERT TABLE ${TABLE_NAME}
+INSERT INTO TABLE ${TABLE_NAME}
 select MergeJSON(d.json, source_table.json, ${JSON_PATH}) as result from ${SOURCE_TABLE_NAME} source_table right outer join 
 	(select b.json as json, regexp_replace(trim(c.uri_to_merge),'\\"', '') as uri from
-		(select a.json, split(regexp_replace(if(a.raw_uris_to_merge IS NULL, "", a.uris_to_merge_string),'\\[|\\]','') ,',') as uris_to_merge_array from 
+		(select a.json, split(regexp_replace(if(a.uris_to_merge_string IS NULL, "", a.uris_to_merge_string),'\\[|\\]','') ,',') as uris_to_merge_array from 
 			(select json, get_json_object(json, concat("$.", ${JSON_PATH})) as uris_to_merge_string from 
 				${TARGET_TABLE_NAME} 
 			) a
