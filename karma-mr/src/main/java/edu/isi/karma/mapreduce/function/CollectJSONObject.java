278d84b3f5832e7ac79b310667573ad8878b2f2c
Chengye Yin <ukby.1234@gmail.com>
2014-09-20 22:09:43 -0700

    New JSONObject collectors

---
+++ karma-mr/src/main/java/edu/isi/karma/mapreduce/function/CollectJSONObject.java
@@ -0,0 +1,63 @@
+package edu.isi.karma.mapreduce.function;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CollectJSONObject extends UDF {
+	private static Logger LOG = LoggerFactory.getLogger(MergeJSON.class);
+	public Text evaluate(Text target, Text path) {
+		try {
+			if (path == null)
+				return target;
+			String targetString = target.toString();
+			String pathString = path.toString().replaceAll("\\[.*\\]","");
+			String[] levels = pathString.split("\\.");
+			JSONObject obj = new JSONObject(targetString);
+			JSONArray array = new JSONArray();
+			collectJSONObject(obj, levels, 1, array);
+			return new Text(array.toString());
+		}catch(Exception e) {
+			LOG.error("something wrong",e );
+			return target;
+		}
+	}
+	
+	public void collectJSONObject(JSONObject obj, String[] levels, int i, JSONArray array) {
+		if (i < levels.length && obj.has(levels[i])) {
+			if (i == levels.length - 1) {
+				Object value = obj.get(levels[i]);
+				if (value instanceof JSONArray) {
+					JSONArray t = (JSONArray)value;
+					for (int j = 0; j < t.length(); j++) {
+						array.put(t.get(j));
+					}
+				}
+				else {
+					array.put(value);
+				}
+			}
+			else {
+				Object value = obj.get(levels[i]);
+				if (value instanceof JSONArray) {
+					JSONArray t = (JSONArray)value;
+					for (int j = 0; j < t.length(); j++) {
+						try {
+							JSONObject o = t.getJSONObject(j);
+							collectJSONObject(o, levels, i + 1, array);
+						}catch (Exception e) {
+							
+						}
+					}
+				}
+				else if (value instanceof JSONObject){
+					collectJSONObject((JSONObject)value, levels, i + 1, array);
+				}
+				
+			}
+		}
+	}
+}
