8d6319cfd36519361e1813709ea78ad44fa57f80
mohsen taheriyan <mohsen@isi.edu>
2015-01-08 17:21:37 -0800

    saving research progress - using LOD patterns to learn semantic models

---
+++ karma-research/src/main/java/edu/isi/karma/research/lod/pattern/LODGreedyModelLearner.java
@@ -0,0 +1,175 @@
+package edu.isi.karma.research.lod.pattern;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.jgrapht.graph.DirectedWeightedMultigraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.isi.karma.modeling.alignment.SemanticModel;
+import edu.isi.karma.rep.alignment.Label;
+import edu.isi.karma.rep.alignment.LabeledLink;
+import edu.isi.karma.rep.alignment.Node;
+import edu.isi.karma.rep.alignment.SemanticType;
+import edu.isi.karma.rep.alignment.SemanticType.Origin;
+
+public class LODGreedyModelLearner {
+
+	static Logger logger = LoggerFactory.getLogger(LODGreedyModelLearner.class);
+	Map<String, Pattern> patterns; // patternId to pattern map
+	Map<String, Set<String>> patternIndex; // type to pattern map
+	
+	public LODGreedyModelLearner(String patternDirectoryPath) {
+		logger.info("importing patterns ...");
+		long timeStart = System.currentTimeMillis();
+		patterns = PatternReader.importPatterns(patternDirectoryPath);
+		long timeImportPatterns = System.currentTimeMillis();
+		float elapsedTimeSec = (timeImportPatterns - timeStart)/1000F;
+		logger.info("time to import the patterns: " + elapsedTimeSec + "s");
+
+		if (patterns == null) {
+			logger.error("no pattern imported.");
+			return;
+		}
+		
+		logger.info("creating index over patterns ...");
+		patternIndex = PatternReader.createPatternIndex(patterns.values());
+		long timeIndexPatterns = System.currentTimeMillis();
+		elapsedTimeSec = (timeIndexPatterns - timeImportPatterns)/1000F;
+		logger.info("time to create index for the patterns: " + elapsedTimeSec + "s");
+	}
+	
+	private Set<Pattern> findRelatedPatterns(List<String> types, Map<String, Pattern> patterns, Map<String, Set<String>> patternIndex) {
+		HashSet<Pattern> relatedPatterns = new HashSet<Pattern>();
+		if (types == null || patternIndex == null) {
+			logger.info("either list of the semantic types or the pattern index is empty");
+			return relatedPatterns;
+		}
+		
+		for (String t : types) {
+			Set<String> patternIds = patternIndex.get(t);
+			if (patternIds != null) {
+				for (String patternId : patternIds) {
+					relatedPatterns.add(patterns.get(patternId));
+				}
+			}
+		}
+		return relatedPatterns;
+	}
+	
+	private List<Pattern> sortPatterns(Set<Pattern> patterns, List<String> types) {
+		List<Pattern> patternsList = new ArrayList<Pattern>(patterns);
+		Collections.sort(patternsList, new PatternComparator(types));
+		return patternsList;
+	}
+	
+	
+	private List<Pattern> findMinimalCovering(List<Pattern> sortedPatterns, List<String> types) {
+		//TODO
+		return null;
+	}
+	
+	private DirectedWeightedMultigraph<Node, LabeledLink> combinePatterns(List<Pattern> patterns) {
+		//TODO
+		return null;
+	}
+	
+	public SemanticModel learn(List<SemanticType> semanticTypes) {
+		
+		if (patterns == null || patternIndex == null) {
+			logger.error("no pattern/patternIndex found.");
+			return null; 
+		}
+
+		if (semanticTypes == null || semanticTypes.isEmpty()) {
+			logger.error("semantic type list is empty.");
+			return null; 
+		}
+
+		List<String> types = new LinkedList<String>(); 
+		for (SemanticType st : semanticTypes) {
+			String domain = st.getDomain() == null ? null : st.getDomain().getUri();
+			if (domain == null) {
+				logger.warn("the domain is null for the semantic type: " + st.getModelLabelString());
+				continue;
+			}
+			types.add(domain);
+		}
+		if (types.isEmpty()) {
+			logger.error("semantic type list is empty.");
+			return null;
+		}
+
+		logger.info("finding related patterns ...");
+		long start = System.currentTimeMillis();
+		Set<Pattern> relatedPatterns = findRelatedPatterns(types, patterns, patternIndex);
+		long timeFindRelatedPatterns = System.currentTimeMillis();
+		float elapsedTimeSec = (timeFindRelatedPatterns - start)/1000F;
+		logger.info("time to find related patterns: " + elapsedTimeSec + "s");
+
+		if (relatedPatterns == null || relatedPatterns.isEmpty()) {
+			logger.info("no related pattern found for the source.");
+			return null;
+		}
+		
+		logger.info("sorting related patterns ...");
+		List<Pattern> sortedPatterns = sortPatterns(relatedPatterns, types);
+		long timeSortPatterns = System.currentTimeMillis();
+		elapsedTimeSec = (timeSortPatterns - timeFindRelatedPatterns)/1000F;
+		logger.info("time to sort related patterns: " + elapsedTimeSec + "s");
+
+		logger.info("finding minimal set of patterns with maximum covering of source types ...");
+		List<Pattern> minimalSet = findMinimalCovering(sortedPatterns, types);
+		long timeFindMinimalCover = System.currentTimeMillis();
+		elapsedTimeSec = (timeFindMinimalCover - timeSortPatterns)/1000F;
+		logger.info("time to find minimal set: " + elapsedTimeSec + "s");
+
+		if (minimalSet == null || minimalSet.isEmpty()) {
+			logger.error("cannot find a pattern set that covers at least one source type, this should not conceptually happen if we have even one related pattern");
+			return null;
+		}
+		
+		logger.info("combining patterns ...");
+		DirectedWeightedMultigraph<Node, LabeledLink> graph = combinePatterns(minimalSet);
+		long timeCombinePatterns = System.currentTimeMillis();
+		elapsedTimeSec = (timeCombinePatterns - timeFindMinimalCover)/1000F;
+		logger.info("time to combine patterns: " + elapsedTimeSec + "s");
+
+		if (graph == null || graph.edgeSet().isEmpty()) {
+			logger.error("graph is null, this should not conceptually happen if we have even one related pattern");
+			return null;
+		}
+
+		SemanticModel sm = new SemanticModel("", graph);
+		return sm;
+
+	}
+	
+	public static void main(String[] args) {
+		
+		String patternDirectoryPath = "/Users/mohsen/Dropbox/Source Modeling/datasets/lod-bm-sample/patterns/";
+		LODGreedyModelLearner ml = new LODGreedyModelLearner(patternDirectoryPath);
+		
+		// get list of semantic types for a source
+		List<SemanticType> types = new ArrayList<SemanticType>();
+		SemanticType st1 = new SemanticType("", null, new Label("http://erlangen-crm.org/current/E39_Actor"), Origin.User, 1.0);
+		SemanticType st2 = new SemanticType("", null, new Label("http://erlangen-crm.org/current/E22_Man-Made_Object"), Origin.User, 1.0);
+		SemanticType st3 = new SemanticType("", null, new Label("http://erlangen-crm.org/current/E21_Person"), Origin.User, 1.0);
+		SemanticType st4 = new SemanticType("", null, new Label("http://erlangen-crm.org/current/E55_Type"), Origin.User, 1.0);
+		SemanticType st5 = new SemanticType("", null, new Label("http://www.w3.org/2004/02/skos/core#Concept"), Origin.User, 1.0);
+		types.add(st1); 
+		types.add(st2);
+		types.add(st3);
+		types.add(st4);
+		types.add(st5);
+
+		SemanticModel sm = ml.learn(types);
+		
+	}
+}
