ac6b619b48f0857b4dc9905d3a1d4b4d0691de4f
Chengye Yin <ukby.1234@gmail.com>
2015-03-05 10:45:51 -0800

    into static reflection

--- karma-mr/src/main/java/edu/isi/karma/mapreduce/inputformat/AvroBatchRecordReader.java
+++ karma-mr/src/main/java/edu/isi/karma/mapreduce/inputformat/AvroBatchRecordReader.java
@@ -1,41 +1,21 @@
 package edu.isi.karma.mapreduce.inputformat;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.nio.charset.Charset;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.avro.Schema;
 import org.apache.avro.mapreduce.AvroRecordReaderBase;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.InputSplit;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AvroBatchRecordReader<T> extends AvroRecordReaderBase<Text, Text, T>{
 
 	private static final int batchSize = 10000;
-	private static Logger LOG = LoggerFactory.getLogger(AvroBatchRecordReader.class);
 	protected AvroBatchRecordReader(Schema readerSchema) {
 		super(readerSchema);
 	}
 	List<JSONObject> data = new LinkedList<JSONObject>();
-	
-	@Override
-	public void initialize(InputSplit inputSplit, TaskAttemptContext context)
-		      throws IOException, InterruptedException {
-		try {
-			Field defaultCharsetField = Charset.class.getDeclaredField("defaultCharset");
-			defaultCharsetField.setAccessible(true);
-			defaultCharsetField.set(null, Charset.forName("UTF-8"));
-		} catch (Exception e) {
-			LOG.error("something wrong", e);
-		} 
-		super.initialize(inputSplit, context);
-	}
 
 	@Override
 	public Text getCurrentKey() throws IOException, InterruptedException {
--- karma-mr/src/main/java/edu/isi/karma/mapreduce/inputformat/AvroKeyBatchInputFormat.java
+++ karma-mr/src/main/java/edu/isi/karma/mapreduce/inputformat/AvroKeyBatchInputFormat.java
@@ -1,6 +1,8 @@
 package edu.isi.karma.mapreduce.inputformat;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
 
 import org.apache.avro.Schema;
 import org.apache.avro.mapreduce.AvroJob;
@@ -9,15 +11,28 @@ import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AvroKeyBatchInputFormat<T> extends FileInputFormat<Text, Text> {
 
+	private static Logger LOG = LoggerFactory.getLogger(AvroKeyBatchInputFormat.class);
 	@Override
 	public RecordReader<Text, Text> createRecordReader(
 			InputSplit split, TaskAttemptContext context) throws IOException,
 			InterruptedException {
 		Schema readerSchema = AvroJob.getInputKeySchema(context.getConfiguration());
 		return new AvroBatchRecordReader<T>(readerSchema);
+	}
+	
+	static {
+		try {
+			Field defaultCharsetField = Charset.class.getDeclaredField("defaultCharset");
+			defaultCharsetField.setAccessible(true);
+			defaultCharsetField.set(null, Charset.forName("UTF-8"));
+		} catch (Exception e) {
+			LOG.error("something wrong", e);
+		} 
 	}
 
 }
