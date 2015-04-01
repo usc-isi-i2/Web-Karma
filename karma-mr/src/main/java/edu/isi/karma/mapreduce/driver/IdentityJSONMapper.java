dc0aab36f2a2afeb592b692c153ee2c86485a068
Chengye Yin <ukby.1234@gmail.com>
2015-03-31 18:31:51 -0700

    change to identityjsonmapper

--- karma-mr/src/main/java/edu/isi/karma/mapreduce/driver/IdentityJSONMapper.java
+++ karma-mr/src/main/java/edu/isi/karma/mapreduce/driver/IdentityJSONMapper.java
@@ -11,19 +11,21 @@ import org.slf4j.LoggerFactory;
 
 public class IdentityJSONMapper extends Mapper<Writable, Text, Text, Text> {
 	private static Logger LOG = LoggerFactory.getLogger(IdentityJSONMapper.class);
-
+	private Text reusableKey = new Text();
 	@Override
 	public void map(Writable key, Text value, Context context) throws IOException, InterruptedException {
 		try {
 			JSONObject obj = new JSONObject(value.toString());
 			if (obj.has("uri")) {
-				context.write(new Text(obj.getString("uri")), value);
+				reusableKey.set(obj.getString("uri"));
+				context.write(reusableKey, value);
 			}
 			else if (obj.has("@id")) {
-				context.write(new Text(obj.getString("@id")), value);
+				reusableKey.set(obj.getString("@id"));
+				context.write(reusableKey, value);
 			}
 			else {
-				context.write(new Text(obj.toString()), value);
+				context.write(value, value);
 			}
 		}catch(Exception e) {
 			LOG.error("something is wrong", e);
