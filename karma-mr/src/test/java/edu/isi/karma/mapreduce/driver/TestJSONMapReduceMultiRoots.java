fffb8d5675d2b931c8de062ce8ee7b5c574b99e3
jasonslepicka <jasonslepicka@gmail.com>
2015-09-14 19:35:34 -0700

    Cleaned up Multi Roots
    Wasn’t able to get distributed cache to work.  Instead just hard coded
    the target/test-classes directory for the model file location.

--- karma-mr/src/test/java/edu/isi/karma/mapreduce/driver/TestJSONMapReduceMultiRoots.java
+++ karma-mr/src/test/java/edu/isi/karma/mapreduce/driver/TestJSONMapReduceMultiRoots.java
@@ -1,5 +1,6 @@
 package edu.isi.karma.mapreduce.driver;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -11,15 +12,16 @@ import net.sf.json.JSONSerializer;
 import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.MRJobConfig;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
 import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.apache.hadoop.mrunit.types.Pair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.apache.hadoop.mrunit.types.Pair;
 
 public class TestJSONMapReduceMultiRoots extends TestRDFMapReduce {
 
@@ -51,22 +53,25 @@ public class TestJSONMapReduceMultiRoots extends TestRDFMapReduce {
 		conf.set("read.karma.config", "true");
 		String string = TestJSONMapReduceMultiRoots.class.getClassLoader().getResource("karmaconfig.json").toURI().toString();
 		conf.set("karma.config.file", string);
-		mapDriver.addCacheFile(TestJSONMapReduceMultiRoots.class.getClassLoader().getResource("people-model.ttl").toURI().toString());
+		String modelFileString = new File(TestJSONMapReduceMultiRoots.class.getClassLoader().getResource("people-model.ttl").toURI()).getAbsolutePath();
+		mapReduceDriver.addCacheFile(modelFileString);
+		System.out.println(conf.get(MRJobConfig.CACHE_FILES));
 		conf.set("karma.input.type", "JSON");
-		conf.set("model.uri", TestJSONMapReduceMultiRoots.class.getClassLoader().getResource("calguns-model.ttl").toURI().toString());
-		conf.set("rdf.generation.root", "http://memexproxy.com/ontology/Thread1");
+		conf.set("model.uri", TestJSONMapReduceMultiRoots.class.getClassLoader().getResource("people-model.ttl").toURI().toString());
+		
 		JSONArray jObj = (JSONArray) JSONSerializer.toJSON(IOUtils.toString(TestJSONMapReduceMultiRoots.class.getClassLoader().getResourceAsStream("data/peoplev2.json")));
 		
 		List<Pair<Writable,Text>> inputs = new ArrayList<Pair<Writable,Text>>();
 		
 		for(int i=0;i<jObj.size();i++){
 		
-			inputs.add(new Pair(new Text(jObj.getJSONObject(i).getString("url")), new Text(jObj.getString(i))));
+			inputs.add(new Pair<Writable,Text>(new Text(jObj.getJSONObject(i).getString("url")), new Text(jObj.getString(i))));
 		}
 		
 		mapReduceDriver.addAll(inputs);
 		
 		mapReduceDriver.addAllOutput(getPairsFromFile("output/people.output.json"));
+		
 		mapReduceDriver.runTest();
 		
 	}
