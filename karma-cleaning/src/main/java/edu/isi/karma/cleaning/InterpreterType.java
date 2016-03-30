7d9ee1b07e28f0d9b3cf21fab6accbaa466de24d
Dipsy Kapoor <dipsykapoor@gmail.com>
2016-03-30 11:44:54 -0700

    remove karma-cleaning

--- karma-cleaning/src/main/java/edu/isi/karma/cleaning/InterpreterType.java
+++
@@ -1,9 +0,0 @@
-package edu.isi.karma.cleaning;
-
-public interface InterpreterType {
-	public void func(String name, Object x);
-
-	public String execute(String value);
-
-	public String execute_debug(String value);
-}
