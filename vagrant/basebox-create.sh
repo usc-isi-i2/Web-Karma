9f42edaa1eda583c56c00423174d512b01fac44e
Giuseppe Futia <giuseppe.futia@gmail.com>
2017-03-04 12:31:43 +0100

    Update maven repository

--- vagrant/basebox-create.sh
+++ vagrant/basebox-create.sh
@@ -6,10 +6,10 @@ sudo yum -y groupinstall 'Development Tools'
 sudo yum -y install wget
 
 cd /tmp
-sudo wget http://apache-mirror.rbc.ru/pub/apache/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz
-sudo tar -xvzpf apache-maven-3.3.3-bin.tar.gz
-sudo mv apache-maven-3.3.3 /usr/local/
-sudo ln -s /usr/local/apache-maven-3.3.3/bin/* /usr/local/bin/
+sudo wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+sudo tar -xvzpf apache-maven-3.3.9-bin.tar.gz
+sudo mv apache-maven-3.3.9 /usr/local/
+sudo ln -s /usr/local/apache-maven-3.3.9/bin/* /usr/local/bin/
 
 cd /home/vagrant/
 git clone https://github.com/usc-isi-i2/Web-Karma.git
